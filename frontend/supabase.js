d26ff7c62a9153a8b5da74f7f30631867a43586d
Guigui26-code <glmwork@hotmail.com>
2026-09-18 09:03:24 +0400

    ajout de la connexion supabase

--- frontend/supabase.js
+++ frontend/supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
+const SUPABASE_URL = "https://rjkoqwbcferftidiwcye.supabase.co/rest/v1/";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_9qK2VHutENspF-sq3vez-Q_lsyn-RQq";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
